c71cd194e47a698e331c5c9e073311accd60e46e
LaurenERusnak <rusnakle@appstate.edu>
2022-02-03 16:42:57 -0500

    add github commit

--- gitconfig_2_3.R
+++ gitconfig_2_3.R
@@ -1,11 +1,12 @@
 # configure commit name and email address
 library(usethis)
-use_git_config(user.name = "Jill Thomley", user.email = "thomleyje@appstate.edu")
+use_git_config(user.name = "LaurenERusnak", user.email = "rusnakle@appstate.edu")
 
 # create your personal access token (PAT)
 library(usethis)
-create_github_token()
+create_github_token() 
 
 # add/change personal access token (PAT)
 library(gitcreds)
-gitcreds_set()
+  gitcreds_set()
+
